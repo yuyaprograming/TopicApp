ed3a20d04e1b0ffa66037a0e67092477e8997632
yuyaprograming <ienjoyschoollife@gmail.com>
2020-08-21 13:49:33 +0900

    話題設定画面の途中まで。

--- app/src/main/java/jp/techacademy/date/yuuya/topicapp/FifthActivity.kt
+++ app/src/main/java/jp/techacademy/date/yuuya/topicapp/FifthActivity.kt
@@ -1,5 +1,6 @@
 package jp.techacademy.date.yuuya.topicapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -12,6 +13,11 @@ class FifthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fifth)
+
+        button9.setOnClickListener {
+            val intent = Intent(this, SixthActivity::class.java)
+            startActivity(intent)
+        }
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
---
+++ app/src/main/java/jp/techacademy/date/yuuya/topicapp/SixthActivity.kt
@@ -0,0 +1,21 @@
+package jp.techacademy.date.yuuya.topicapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sixth.*
+
+class SixthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sixth)
+
+        button10.setOnClickListener {
+
+        }
+
+        button11.setOnClickListener {
+
+        }
+    }
+}
